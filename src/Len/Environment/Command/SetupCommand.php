eec73bd8faf743b3d6e1640221c77dbbc8c84837
Len Lorijn <len.lorijn@mediact.nl>
2015-03-23 08:55:48 +0100

    better command divisions

--- src/Len/Environment/Command/SetupCommand.php
+++ src/Len/Environment/Command/SetupCommand.php
@@ -12,55 +12,50 @@ use Symfony\Component\Console\Input\StringInput;
 
 class SetupCommand extends AbstractMagentoCommand {
 
-  /**
-   * Configure the command
-   */
-  public function configure(){
-    $this->setName('len:setup')
-      ->addArgument('projectname', InputArgument::REQUIRED, "The name of the project")
-      ->setDescription('Set up a new dev env');
-  }
+    /**
+     * Configure the command
+     * @return void
+     */
+    public function configure(){
+        $this->setName('len:setup')
+            ->addArgument('projectname', InputArgument::REQUIRED, "The name of the project")
+            ->setDescription('Set up a new dev env');
+    }
 
     /**
-    * @param \Symfony\Component\Console\Input\InputInterface $input
+     * Execute Setup Command
+     *
+    * @param \Symfony\Component\Console\Input\InputInterface   $input
     * @param \Symfony\Component\Console\Output\OutputInterface $output
+    *
     * @return int|void
     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Inputs to use in script
+        $projectname = $input->getArgument('projectname');
 
-      $projectname = $input->getArgument('projectname');
+        // Make commands to use in the process of setting up
+        $gitCommand = new StringInput('len:setup:git '.$projectname);
+        $modmanCommand = new StringInput('len:setup:modman');
 
-      $output->writeln("<info>Making dir /var/www/".$projectname." and going there</info>");
+        // Ensure that n98-magerun doesn't stop after other commands
+        $this->getApplication()->setAutoExit(false);
 
-      $output->writeln("<info>Cloning repo: git@mediact.git.beanstalkapp.com:/mediact/".$projectname.".git </info>");
-      passthru('git clone git@mediact.git.beanstalkapp.com:/mediact/'.$projectname.'.git '.$projectname);
+        // Creating place for project to live
+        $output->writeln("<info>Making dir /var/www/".$projectname." and going there</info>");
+        mkdir('/var/www/'.$projectname);
+        chdir('/var/www/'.$projectname);
 
-      $output->writeln("<info>initialising git git flow and attempting to fetch repo</info>");
-      chdir('/var/www/'.$projectname);
-      passthru('git flow init');
+        // Run other commands
+        $this->getApplication()->run($gitCommand, $output);
+        $this->getApplication()->run($modmanCommand, $output);
+        
+        // Apache needs to be done externally as sudo
+        $output->writeln("<info>Going to make Apache config</info>");
+        passthru('sudo n98-magerun len:config:apache '.$projectname);
 
-      $output->writeln("<info>Initialising modman</info>");
-      passthru('modman init');
-
-      $output->writeln("<info>Cloning standard dev tools in to repo</info>");
-      passthru('modman clone https://github.com/AOEpeople/Aoe_Profiler.git');
-      passthru('modman clone https://github.com/AOEpeople/Aoe_TemplateHints.git');
-      passthru('modman clone git@mediact.git.beanstalkapp.com:/mediact/commercebug.git');
-
-      $output->writeln("<info>Going to make Apache config</info>");
-      passthru('sudo n98-magerun len:config:apache '.$projectname);
-
-      //run other setup commands
-      //$input = new StringInput('len:config:apache '.$projectname);
-
-      // ensure that n98-magerun doesn't stop after first command
-      //$this->getApplication()->setAutoExit(false);
-     
-      // with output
-      //$this->getApplication()->run($input, $output);
-      
-      // reactivate auto-exit
-      //$this->getApplication()->setAutoExit(true);
+        // reactivate auto-exit
+        $this->getApplication()->setAutoExit(true);
     }
 }
